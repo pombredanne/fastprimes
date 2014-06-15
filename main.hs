995e60644dc1861b0e4687c04480b817d3f08109
Trevor Pottinger <tpott@fb.com>
2014-06-15 10:25:27 -0700

    Haskell is hard, switching to python

--- main.hs
+++ main.hs
@@ -5,5 +5,15 @@
 
 module Main where
 
+import Text.Printf
+
 main :: IO ()
-main = putStrLn "sup buddy"
+main = do
+  generateConfig 3 7 4
+
+generateConfig :: Integer -> Integer -> Integer -> IO ()
+generateConfig nFuncs filterMagnitude maxNumMagnitude = 
+  do
+    -- printf "Creating a Bloom filter with %d hash functions\n" nFuncs
+    printf "  with a filter of size %d" $ (2 ^ filterMagnitude :: Int)
+    -- printf " and max number of %d\n" $ (2 ^ (2 ^ maxNumMagnitude)) - 1
